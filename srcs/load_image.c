b7630b0bcae866de2ee0bc56230333f9f594dbdb
Guillaume PINCHON <gpinchon@e3r7p14.42.fr>
2017-02-08 17:52:02 +0100

    Corrected load_image behavior

--- srcs/load_image.c
+++ srcs/load_image.c
@@ -6,7 +6,7 @@
 /*   By: gpinchon <gpinchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/06 22:18:47 by gpinchon          #+#    #+#             */
-/*   Updated: 2017/02/06 21:54:27 by gpinchon         ###   ########.fr       */
+/*   Updated: 2017/02/08 17:51:39 by gpinchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ void	*load_image_file(void *framework, const char *path)
 	GSTRUCT		*istruct;
 	SDL_Surface	*bmp;
 
-	istruct = new_object(sizeof(t_img));
 	bmp = SDL_LoadBMP(path);
 	FRAMEWORK_DEBUG(!bmp, SDL_ERROR, "load_image_file");
 	if (!bmp)
-	{
-		free(istruct->data);
-		free(istruct);
 		return (NULL);
-	}
 	SDL_LockSurface(bmp);
+	istruct = new_object(sizeof(t_img));
 	if (bmp->format->BitsPerPixel == 32)
 	{
 		SDL_FreeFormat(bmp->format);
